dd3df92bafee6f9e9929195d190feadaacc1ef0a
Me7atonin <sammyalex2006@gmail.com>
2024-12-11 12:35:57 -0800

    made difficulty buttons send you to levels

--- BulletHell/Assets/Scripts/Menu.cs
+++ BulletHell/Assets/Scripts/Menu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class WinScript : MonoBehaviour
+public class Menu : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
